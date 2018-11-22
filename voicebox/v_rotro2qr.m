a4f622bbcf52a143d88b1de241446130efde5bd8
Mike Brookes <mik.brookes@gmail.com>
2018-11-22 11:13:21 +0000

    Added output plot if no output arguments given. Improved efficiency of quaternion sign-normalization. Corrected typos in comments.

--- voicebox/v_rotro2qr.m
+++ voicebox/v_rotro2qr.m
@@ -1,19 +1,19 @@
 function q=v_rotro2qr(r)
-%ROTRO2QR converts a 3x3 v_rotation matrix to a real quaternion
+%ROTRO2QR converts a 3x3 rotation matrix to a real quaternion
 % Inputs: 
 %
-%     R(3,3)   Input v_rotation matrix
+%     R(3,3)   Input rotation matrix
 %
 % Outputs: 
 %
 %     Q(4,1)   normalized real-valued quaternion
 %
-% In the quaternion representation of a v_rotation, and q(1) = cos(t/2) 
-% where t is the angle of v_rotation in the range 0 to 2pi
-% and q(2:4)/sin(t/2) is a unit vector lying along the axis of v_rotation
-% a positive v_rotation about [0 0 1] takes the X axis towards the Y axis.
+% In the quaternion representation of a rotation, and q(1) = cos(t/2) 
+% where t is the angle of rotation in the range 0 to 2pi
+% and q(2:4)/sin(t/2) is a unit vector lying along the axis of rotation
+% a positive rotation about [0 0 1] takes the X axis towards the Y axis.
 
-%      Copyright (C) Mike Brookes 2007
+%      Copyright (C) Mike Brookes 2007-2018
 %      Version: $Id: v_rotro2qr.m 10865 2018-09-21 17:22:45Z dmb $
 %
 %   VOICEBOX is a MATLAB toolbox for speech processing.
@@ -35,10 +35,10 @@ function q=v_rotro2qr(r)
 %   Free Software Foundation, Inc.,675 Mass Ave, Cambridge, MA 02139, USA.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% in the comments below, t is the v_rotation angle, a is the v_rotation axis
+% in the comments below, t is the rotation angle, a is the rotation axis
 q=zeros(4,1);
 t = 1 + r(1) + r(5) + r(9);     % 2(1+cos(t)) = 4(cos(t/2))^2 = 4 q(1)^2
-if t>1                       % for v_rotation angles less than 120 degrees
+if t>1                       % for rotation angles less than 120 degrees
     s = sqrt(t) * 2;            % 4 cos(t/2) = 2 sin(t)/sin(t/2)
     q(2) = ( r(6) - r(8) ) / s;
     q(3) = ( r(7) - r(3) ) / s;
@@ -63,9 +63,11 @@ else                                             % row 3
     q(4) = 0.25 * s;
     q(1) = (r(2) - r(4) ) / s;
 end
-f=find(q~=0);
-if (q(f(1))<0), q=-q; end           % force positive
+q=q*(2*(q(find(q~=0,1))>0)-1); % force leading coefficient to be positive
 if abs(q'*q-1)>1e-8
     error('Input to rotro2qr must be a rotation matrix with det(r)=+1');
+end
+if ~nargout
+    v_rotqr2ro(q); % plot a cube
 end
      
