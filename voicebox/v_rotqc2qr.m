5337065c542cb2a45e3866d8e63e4c763e2f401c
Mike Brookes <mik.brookes@gmail.com>
2018-11-22 11:04:39 +0000

    Vecorized for arbitrary input array dimensions. Added output plot if no output arguments given.

--- voicebox/v_rotqc2qr.m
+++ voicebox/v_rotqc2qr.m
@@ -3,16 +3,16 @@ function qr=v_rotqc2qr(qc)
 %
 % Inputs: 
 %
-%     QC(2m,n)   mxn matrix of complex-valued quaternions
+%     QC(2m,...)   array of complex-valued quaternions
 %
 % Outputs: 
 %
-%     QR(4m,n)   mxn matrix of real-valued quaternions
+%     QR(4m,...)   array of real-valued quaternions
 %
 % The complex-valued quaternion [r+j*b  a+j*c] becomes [r a b c]
 
 % 
-%      Copyright (C) Mike Brookes 2000-2006
+%      Copyright (C) Mike Brookes 2000-2018
 %      Version: $Id: v_rotqc2qr.m 10865 2018-09-21 17:22:45Z dmb $
 %
 %   VOICEBOX is a MATLAB toolbox for speech processing.
@@ -33,8 +33,15 @@ function qr=v_rotqc2qr(qc)
 %   http://www.gnu.org/copyleft/gpl.html or by writing to
 %   Free Software Foundation, Inc.,675 Mass Ave, Cambridge, MA 02139, USA.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[m,n]=size(qc);
-i=(1:2:2*m)-mod(0:m-1,2);
-qr=zeros(2*m,n);
-qr(i,:)=real(qc);
-qr(i+2,:)=imag(qc);
+persistent a b
+if isempty(a)
+    a=[1 3 2 4];
+end
+s=size(qc);
+s(1)=2*s(1); % each complex number needs two reals
+qr=reshape([real(qc(:).'); imag(qc(:).')],4,[]);
+qr=reshape(qr(a,:),s);
+if ~nargout
+    qr=qr(1:4); % just select the first one
+    v_rotqr2ro(qr(:)); % plot a rotated cube
+end
