6f7249d2dca9e214f5e46551ab650e45b59b87eb
Mike Brookes <mik.brookes@gmail.com>
2019-02-06 13:35:23 +0000

    extend to vector arguments

--- voicebox/v_atan2sc.m
+++ voicebox/v_atan2sc.m
@@ -3,9 +3,12 @@ function [s,c,r,t]=v_atan2sc(y,x)
 %
 % Outputs:
 %    s    sin(t) where tan(t) = y/x
-%    C    cos(t) where tan(t) = y/x
+%    c    cos(t) where tan(t) = y/x
 %    r    sqrt(x^2 + y^2)
 %    t    arctan of y/x
+%
+% Note y and x can be arrays but must be the same size. The outputs will
+% all be the same size as y.
 
 %      Copyright (C) Mike Brookes 2007
 %      Version: $Id: v_atan2sc.m 10865 2018-09-21 17:22:45Z dmb $
@@ -29,32 +32,35 @@ function [s,c,r,t]=v_atan2sc(y,x)
 %   Free Software Foundation, Inc.,675 Mass Ave, Cambridge, MA 02139, USA.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-t=NaN;
-if y == 0
-    t=(x<0);
-    c=1-2*t;
-    s=0;
-    r=abs(x);
-    t=t*pi;
-elseif x == 0
-    s=2*(y>=0)-1;
-    c=0;
-    r=abs(y);
-    t=s*0.5*pi;
-elseif (abs(y) > abs(x))
-    q = x/y;
-    u = sqrt(1+q^2)*(2*(y>=0)-1);
-    s = 1/u;
-    c = s*q;
-    r = y*u;
-else
-    q = y/x;
-    u = sqrt(1+q^2)*(2*(x>=0)-1);
-    c = 1/u;
-    s = c*q;
-    r = x*u;
+sz=size(y);
+s=zeros(sz);
+c=NaN(sz);
+r=zeros(sz);
+t=NaN(sz);
+m=y==0;
+if any(m(:)) % handle case when y=0 and possibly x=0 also
+    t(m)=(x(m)<0);
+    c(m)=1-2*t(m);
+    r(m)=abs(x(m));
+    t(m)=t(m)*pi;
 end
-if nargout>3 && isnan(t)
-    t=atan2(s,c);
+m=abs(y)>abs(x) & isnan(c);
+if any(m(:))
+    q = x(m)./y(m);
+    u = sqrt(1+q.^2).*sign(y(m)); % avoids underflow even if x and y are very small
+    s(m) = 1./u;
+    c(m) = s(m).*q;
+    r(m) = y(m).*u;
+end
+m=isnan(c);
+if any(m(:))
+    q = y(m)./x(m);
+    u = sqrt(1+q.^2).*sign(x(m));
+    c(m) = 1./u;
+    s(m) = c(m).*q;
+    r(m) = x(m).*u;
+end
+m=isnan(t);
+if nargout>3 && any(m(:))
+    t(m)=atan2(s(m),c(m));
 end
