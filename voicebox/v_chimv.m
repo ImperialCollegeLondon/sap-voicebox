b93022ba752b70d2e9ed064304b7515105f5a90a
Mike Brookes <mik.brookes@gmail.com>
2020-07-18 19:28:48 +0100

    Updated comments to include a reference to the source of the expression for the mean

--- voicebox/v_chimv.m
+++ voicebox/v_chimv.m
@@ -13,20 +13,27 @@ function [m,v]=v_chimv(n,l,s)
 % z=sqrt(x'*x) has a chi distributon with n degrees of freedom and non-centrality parameter
 % l=sqrt(c'*c). The mean and variance of a chi distribution are given precisely by
 %
-%     m = sqrt(2)*exp(gammaln(0.5*n+0.5)-gammaln(0.5*n))*hypergeom(-0.5,0.5*n,-0.5*l^2)
-%       = sqrt(pi/2) L(0.5,0.5*n-1,-0.5*l^2)
-%     v = n+l^2-m^2
+%     m = sqrt(2)*exp(gammaln(0.5*n+0.5)-gammaln(0.5*n))*hypergeom(-0.5,0.5*n,-0.5*(l/s)^2)*s
+%       = sqrt(pi/2) L(0.5,0.5*n-1,-0.5*(l/s)^2)*s
+%     v = n*s^2+l^2-m^2
 %
 % where L(n,a,x) is the generalized Laguerre polynomial L_n^{(a)}(x) but this is very slow
 % to calculate so this routine approximates these expressions.
+% The expressions are from [1]  together with the following identities taken
+% from [2] or [3]: 13.2.39, 13.6.19, 5.2.5, 5.4.1, 5.5.1, 5.4.6.
 %
 % For n=1, the accuracy is high; for n>1, accuracy improves with increasing n.
 % Accuracy is worst when the non-centrality parameter, l, is close to s*sqrt(n).
 % Worst case errors as a function of n are:
 %                       n:    1       2      3       5      10
 %   worst case error in m:  1e-15   0.007  0.004  0.0015  0.0005
+%
+% References:
+% [1]	J. H. Park. Moments of the generalized Rayleigh distribution. Quarterly of Applied Mathematics, 19: 45–49, 1961.
+% [2]	F. W. J. Olver, D. W. Lozier, R. F. Boisvert, and C. W. Clark, editors. NIST Handbook of Mathematical Functions. CUP, 2010. ISBN 978-0-521-14063-8.
+% [3]	F. W. J. Olver, D. W. Lozier, R. F. Boisvert, and C. W. Clark, editors. NIST Digital Library of Mathematical Functions. 2010-2014. URL http://dlmf.nist.gov/.
 
-%      Copyright (C) Mike Brookes 2014
+%      Copyright (C) Mike Brookes 2014-2020
 %      Version: $Id: v_chimv.m 4969 2014-08-05 18:24:30Z dmb $
 %
 %   VOICEBOX is a MATLAB toolbox for speech processing.
