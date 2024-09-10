191248d0b6905aaa11de0c50c569d659a8bcc320
Mike Brookes <mik.brookes@gmail.com>
2024-09-10 18:47:11 +0100

    Included error messages for parameter check conditions + updated comments

--- voicebox/v_paramsetch.m
+++ voicebox/v_paramsetch.m
@@ -1,17 +1,17 @@
 function p=v_paramsetch(d,q,m,c,t)
 %V_PARAMSETCH update and check parameter values p=(d,q,m,c,t)
 % Usage: (1) function x=func(y,q)
-%            d=struct('a',1,'b',2,'c',3); % default parameters
-%            p=v_paramsetch(d,q); % update selected parameters
+%            d=struct('a',1,'b',2,'c',3);   % default parameters
+%            p=v_paramsetch(d,q);           % update selected parameters
 %
 %        (2) function x=func(y,q)
-%            d=struct('a',1,'b',2,'c',3); % default parameters
+%            d=struct('a',1,'b',2,'c',3);   % default parameters
 %            c={'p.a>0 && p.a<5','p.b>0'};
-%            p=v_paramsetch(d,q,'E',c); % check parameter ranges
+%            p=v_paramsetch(d,q,'E',c);     % check parameter ranges
 %
 %        (3) t={'a','description of parameter a';'c','and of parameter c'}
-%            p=v_paramsetch(d,q,'l',c,t); % list values with optional descritions
-%                                       % '-','*','+' indicates default, updated and new fields
+%            p=v_paramsetch(d,q,'l',c,t);   % list values with optional descrpitions
+%                                           % '-','*','+' indicates default, updated and new fields
 %
 %  Inputs:
 %       d  default parameter structure
@@ -23,7 +23,8 @@ function p=v_paramsetch(d,q,m,c,t)
 %           'e' print errors but don't exit
 %           'E' print errors and exit
 %           'l' list fields and their values (default if no output)
-%       c  cell array with parameter checking conditions e.g. 'p.a>3' (use p for structure name)
+%       c  cell array with parameter checking conditions and error message; one row per check. All results of command c{*,1) must be true.
+%               e.g. {'p.a>3' 'parameter a must be > 3'} (use p for structure name)
 %       t  cell array with descriptive text for each field in a new row. Either in
 %          the form t(:,*)={'field' 'description'} or a single column of
 %          descriptions in the same order as the fields of d
@@ -31,6 +32,10 @@ function p=v_paramsetch(d,q,m,c,t)
 % Outputs:
 %       p  output parameter structure
 %
+% Bugs/Suggestions:
+% (1) When printing include "=" to show an updated parameter has not changed (might waste a lot of computation)
+% (2) If input d has few entries, it would be faster to loop through d rather than q
+% (3) to save speed, eliminate the dup variable and work it all out when printing
 
 %      Copyright (C) Mike Brookes 2017
 %      Version: $Id: v_paramsetch.m 10865 2018-09-21 17:22:45Z dmb $
@@ -53,44 +58,44 @@ function p=v_paramsetch(d,q,m,c,t)
 %   http://www.gnu.org/copyleft/gpl.html or by writing to
 %   Free Software Foundation, Inc.,675 Mass Ave, Cambridge, MA 02139, USA.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-p=d;                % initialize to the default values
-numerr=0;           % initialize error count
-dn=fieldnames(d);   % default list of parameter fields
-ndn=length(dn);     % number of default parameter fields
-dup=zeros(ndn,1);   % update flags for default fields
+p=d;                                    % initialize to the default values
+numerr=0;                               % initialize error count
+dn=fieldnames(d);                       % default list of parameter fields
+ndn=length(dn);                         % number of default parameter fields
+dup=zeros(ndn,1);                       % update flags for default fields
 % sort out input arguments
 if nargin<5
-    t={'' ''};      % define an empty description array
+    t={'' ''};                          % define an empty description array
     if nargin<4
-        c=cell(0);  % define an empty check condition array       
+        c=cell(0);                      % define an empty check condition array       
         if nargin<3
-            m='';   % define an empty mode string
+            m='';                       % define an empty mode string
         end
     end
 end
 % now update the selected fields
-if nargin>1 && numel(q)>0   % if update argument exists
-    if isstruct(q)          % if update argument is a structure
-        qn=fieldnames(q);   % field names to update
-        addnew=any(m=='a'); % new fields should be added into p
-        adderr=~addnew && any(m=='A');         % new fields constitute an error
-        for i=1:length(qn)
-            fi=qn{i};
-            old=isfield(p,fi); % is this an existing field ?
+if nargin>1 && numel(q)>0               % if update argument exists
+    if isstruct(q)                      % if update argument is a structure
+        qn=fieldnames(q);               % field names to update
+        addnew=any(m=='a');             % new fields should be added into p
+        adderr=~addnew && any(m=='A');  % new fields constitute an error
+        for i=1:length(qn)              % loop through list of fields to update
+            fi=qn{i};                   % get field name to update
+            old=isfield(p,fi);          % is this an existing field ?
             if addnew || old
-                p.(fi)=q.(fi);
+                p.(fi)=q.(fi);          % set new field value
             end
             if old
                 dup(find(strcmp(fi,dn),1))=1; % indicate field is updated
             end
             if adderr && ~old
                 fprintf(2,'%s is an unknown parameter field\n',fi);
-                numerr=numerr+1; % increment error count
+                numerr=numerr+1;        % increment error count
             end
         end        
-    else                            % else update argument is a matrix
-        nq=min(size(q,1),ndn);      % number of fields to update
-        dup(1:nq)=1;                % indicate which fields are updated
+    else                                % else update argument is a matrix
+        nq=min(size(q,1),ndn);          % number of fields to update
+        dup(1:nq)=1;                    % indicate which fields are updated
         for i=1:nq
             p.(dn{i})=q(i,:);
         end
@@ -101,11 +106,15 @@ if nargin>1 && numel(q)>0   % if update argument exists
     end
 end
 % Apply parameter checks
-if any(lower(m)=='e') && numel(c)>0
-    for i=1:numel(c)
-        if any(~eval(c{i}))
+if any(lower(m)=='e') && numel(c)>0     % check if any checks are specified
+    for i=1:size(c,1)                   % one check per row of c
+        if any(~eval(c{i,1}))           % perform the check
             numerr=numerr+1;
-            fprintf(2,'Parameter check failed: %s\n',c{i});
+            if size(c,1)==1
+                fprintf(2,'Parameter check failed: %s\n',c{i});
+            else
+                fprintf(2,c{i,1});
+            end
         end
     end
 end
