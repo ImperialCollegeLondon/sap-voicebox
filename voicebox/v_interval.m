1afb0116d80fd1b2ed187dc2335fef38d1282613
Mike Brookes <mik.brookes@gmail.com>
2025-07-14 12:50:06 +0100

    Added copyright notice

--- voicebox/v_interval.m
+++ voicebox/v_interval.m
@@ -24,6 +24,28 @@ function [i,f]=v_interval(x,y,m)
 %           f(nx)   f(j)=(x(j)-y(i(j)))/(y(i(j)+1))-y(i(j))) is the fractional position of x(j) within the interval.
 %                   Note that f(j) lies in the range [0,1) provided that y(1) <= x(j) < y(ny)
 %
+
+%	   Copyright (C) Mike Brookes 2025
+%      Version: $Id: v_importsii.m 10865 2018-09-21 17:22:45Z dmb $
+%
+%   VOICEBOX is a MATLAB toolbox for speech processing.
+%   Home page: http://www.ee.ic.ac.uk/hp/staff/dmb/voicebox/voicebox.html
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   This program is free software; you can redistribute it and/or modify
+%   it under the terms of the GNU Lesser General Public License as published by
+%   the Free Software Foundation; either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU Lesser General Public License for more details.
+%
+%   You can obtain a copy of the GNU Lesser General Public License from
+%   https://www.gnu.org/licenses/ .
+%    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargin<3
     m='';
 end
